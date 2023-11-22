551f1cdb75f9549f014bebf905c6e971fe599614
Tumsifu Lema <tom455976@gmail.com>
2023-11-23 00:55:42 +0300

    improved perfomance of the webpage

--- js/main.js
+++ js/main.js
@@ -60,3 +60,24 @@ window.addEventListener("scroll", function () {
 document.querySelector(".back-to-top").addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+//this is for counterup of data
+const counterUp = window.counterUp.default;
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    const el = entry.target;
+    if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+      counterUp(el, {
+        duration: 1000,
+        delay: 10,
+      });
+      el.classList.add("is-visible");
+    }
+  });
+};
+//now for each element the funtion is run
+const counters = document.querySelectorAll(".counter");
+counters.forEach((counter) => {
+  const IO = new IntersectionObserver(callback, { threshold: 1 });
+  IO.observe(counter);
+});
