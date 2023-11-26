f26ad38c0791c5afd58bdd8584019359e4022907
Tumsifu Lema <tom455976@gmail.com>
2023-11-26 20:26:52 +0300

    added new features to the website such as slider animations and looking forward into creating a new page for our services page

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+const tnslider = tns({
+  container: ".slider",
+  slideBy: 1,
+  speed: 700,
+  nav: true,
+  navPosition: "bottom",
+  autoPlay: true,
+  autoPlayTimeout: 7000,
+  autoPlayButtonOutput: false,
+  controlsContainer: "#controls",
+  prevButton: ".previous",
+  nextButton: ".next",
+});
